d42c0f8b7660295090ffdbbe54586c96f7fa7851
dainvinc <vishal.koosuri1@marist.edu>
2017-03-28 00:35:05 +0000

    Identifies valid image files now

--- myScrape.py
+++ myScrape.py
@@ -1,8 +1,9 @@
 import requests
 import urllib
+import string
 from BeautifulSoup import BeautifulSoup
 
-url = 'http://rockefeller100.org/'
+url = 'http://rockefeller100.org/exhibits/show/agriculture'
 response = requests.get(url)
 html = response.content
 #print html
@@ -12,8 +13,8 @@ count = 0
 image_links = [image.get('src') for image in soup.findAll('img')]
         # print image.get('src')
         # count=count+1
-print image_links
-print len(image_links)
+#print image_links
+print "%d image links are found" %(len(image_links))
 #print count, "images are found"
 
 out = open('links.txt', 'w')
@@ -22,8 +23,17 @@ print out
 out.close()
 input_txt = open('links.txt', 'r')
 for link in input_txt:
-        print link
-        url = link
-        image = link.rsplit('/', 1)[1]
-        urllib.urlretrieve(url, image)
+        #image = url.rsplit('/', 1)[1]
+        #urllib.urlretrieve(url, 'static/images'+image)
+        filename = link.rsplit('/')[-1]
+        print filename
+        if 'jpg' in link:
+                print 'image found'
+                try:
+                        urllib.urlretrieve(link, 'static/images/'+filename)
+                except IOError:
+                        print 'yeah'
+        else:
+                print 'not found'
+        
 input_txt.close()
